2c28d31f6b618d90f24e226d781b096b2445ca53
Jose Luis Garcia Marin <josgarmar31@alum.us.es>
2023-03-16 12:26:04 +0100

    FEAT: MODIFICACIONES EN LAS OBTENCIÓN DE PARÁMETROS
    Se ha contemplado aquellos parámetros que son referenciados. Además se ha actualizado el método parseador de yaml a json para que contemple algunas codificacones de caracteres especiales y los objetos de tipo date y datetime.

--- python/main.py
+++ python/main.py
@@ -1,15 +1,15 @@
-from oas_analysis.main_oas_analysis import load_oas, load_parameters
+from oas_analysis.main_oas_analysis import load_oas, load_parameters, search_ref
 
 
 if __name__ == '__main__':
-    path = 'tests_set/pet_clinic.yaml'
+    path = 'tests_set/authentiq_team.yaml'
     oas = load_oas(path)
     path = oas['paths']
 
     for url in path:
         print(f'\n################## {url} ##################')
         for method in path[url]:
-            print(load_parameters(oas, url, method))
+            print(load_parameters(oas['paths'][url][method], oas))
 
 
 
--- python/oas_analysis/main_oas_analysis.py
+++ python/oas_analysis/main_oas_analysis.py
@@ -1,3 +1,4 @@
+import datetime
 import yaml
 import json
 import os
@@ -5,10 +6,15 @@ import os
 from python.classes import Parameter
 
 
+def custom_json_serializer(obj):
+    if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
+
 def yaml2json(path: str):
-    with open(path, 'r') as f:
+    with open(path, 'r', encoding='utf-8') as f:
         data = yaml.safe_load(f)
-    return json.dumps(data)
+    return json.dumps(data, default=custom_json_serializer)
 
 
 def load_oas(path: str):
@@ -27,11 +33,13 @@ def load_oas(path: str):
     return _file
 
 
-def load_parameters(file, url, method):
-    path = file['paths'][url][method]
-    parameters = path.get('parameters', [])
+def load_parameters(method, oas):
+
+    parameters_list = method.get('parameters', [])
     res = []
-    for parameter in parameters:
+    for parameter in parameters_list:
+        if '$ref' in parameter:
+            parameter = search_ref(oas, parameter['$ref'])
         name = parameter.get('name', '')
         query = parameter.get('in', '') == 'query'
         required = parameter.get('required', False)
@@ -41,6 +49,12 @@ def load_parameters(file, url, method):
         res.append(Parameter(name, type, query, required))
     return res
 
+def search_ref(oas, ref):
+    rute = ref.split('/')
+    for i in rute:
+        if i != '#':
+            oas = oas[i]
+    return oas
 
 
 
